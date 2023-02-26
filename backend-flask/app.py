9273ee875383eddaec873eec01727a04c5ed9c06
Felix Pitterling <f.pitterling@gmail.com>
2023-02-26 20:47:38 +0000

    Week 2 Class notes + commands - xray (not live)

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,13 +34,13 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+
+app = Flask(__name__)
+
 # Xray ----------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
-
-
-app = Flask(__name__)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
