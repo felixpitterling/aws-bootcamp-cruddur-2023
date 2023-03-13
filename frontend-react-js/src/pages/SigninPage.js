66a867bdea997aff5940d37fcadf379a35e26ec6
felixpitterling <f.pitterling@gmail.com>
2023-03-13 15:48:50 +0100

    Week 3 Homework Evidence - Required Homework + Backend JWT Auth

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -8,45 +8,189 @@ import { Link } from "react-router-dom";
 import { Auth } from 'aws-amplify';
 import QRCode from 'qrcode.react';
 
+//MFA VERSION
+// export default function SigninPage() {
+
+//   const [email, setEmail] = React.useState('');
+//   const [password, setPassword] = React.useState('');
+//   const [mfa, setMfa] = React.useState('');
+//   const [errors, setErrors] = React.useState('');
+//   const [code, setCode] = React.useState('');
+//   const [mfaactive, setMfaactive] = React.useState(false);
+//   const [gotuser, setGotuser] = React.useState();
+
+
+//   const onsubmitcode = async (event) => {
+//     setErrors('')
+//     event.preventDefault();
+
+//     var dummyuser = gotuser;
+
+//     if(mfaactive == false) {
+//     Auth.verifyTotpToken(dummyuser, mfa)
+//       .then(() => {
+//         // don't forget to set TOTP as the preferred MFA method
+//         Auth.setPreferredMFA(gotuser, 'TOTP');
+//         // gotuser.attributes.setMfaactive("true");
+//         console.log("hey")
+//       })
+//       .catch((e) => {
+//         setErrors(e.message)
+//       });
+//     }
+
+//     try {
+//       await  Auth.confirmSignIn(gotuser, mfa, 'SOFTWARE_TOKEN_MFA') ;
+//       localStorage.setItem("access_token", gotuser.signInUserSession.accessToken.jwtToken)
+//       window.location.href = "/"
+//     } catch (error) {
+//       setErrors(error.message)
+//     }
+
+//   }
+
+//   const onsubmit = async (event) => {
+//     setErrors('')
+//     event.preventDefault();
+
+//     Auth.signIn(email, password)
+//       .then(user => {
+//         setGotuser(user);
+        
+//         try{
+//         if(user.attributes["custom:mfa_active"]) {
+//           setMfaactive(false);
+//         } }
+//         catch {
+//           setMfaactive(true); 
+//         }
+   
+        
+
+//         Auth.setupTOTP(user).then((code) => {
+//           console.log(code)
+//           const str = "otpauth://totp/AWSCognito?secret=" + code;
+//           setCode(str);
+//           console.log(str);
+//         });
+
+//       })
+//       .catch(error => {
+//         if (error.code == 'UserNotConfirmedException') {
+//           window.location.href = "/confirm"
+//         }
+//         setErrors(error.message)
+//         console.log(error.message)
+//       });
+
+//     return false
+//   }
+
+//   const email_onchange = (event) => {
+//     setEmail(event.target.value);
+//   }
+//   const password_onchange = (event) => {
+//     setPassword(event.target.value);
+//   }
+//   const mfa_onchange = (event) => {
+//     setMfa(event.target.value);
+//   }
+
+//   let el_errors;
+//   if (errors) {
+//     el_errors = <div className='errors'>{errors}</div>;
+//   }
+
+//   return (
+//     <article className="signin-article">
+//       <div className='signin-info'>
+//         <Logo className='logo' />
+//       </div>
+
+//       {gotuser && mfaactive == false ? (
+//         <QRCode value={code} />
+//       ) : (
+//         <></>
+//       )}
+
+//       <div className='signin-wrapper'>
+
+//         {!gotuser ? (
+//           <form
+//             className='signin_form'
+//             onSubmit={onsubmit}
+//           >
+//             <h2>Sign into your Cruddur account</h2>
+//             <div className='fields'>
+//               <div className='field text_field username'>
+//                 <label>Email</label>
+//                 <input
+//                   type="text"
+//                   value={email}
+//                   onChange={email_onchange}
+//                 />
+//               </div>
+//               <div className='field text_field password'>
+//                 <label>Password</label>
+//                 <input
+//                   type="password"
+//                   value={password}
+//                   onChange={password_onchange}
+//                 />
+//               </div>
+//             </div>
+//             {el_errors}
+//             <div className='submit'>
+//               <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
+//               <button type='submit'>Sign In</button>
+//             </div>
+
+//           </form>
+//         ) : (
+//           <form
+//             className='signin_form'
+//             onSubmit={onsubmitcode}
+//           >
+//             <h2>Sign into your Cruddur account</h2>
+//             <div className='fields'>
+
+//               <div className='field text_field username'>
+//                 <label>MFA Code</label>
+//                 <input
+//                   type="mfa"
+//                   value={mfa}
+//                   onChange={mfa_onchange}
+//                 />
+//               </div>
+//             </div>
+//             {el_errors}
+//             <div className='submit'>
+//               <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
+//               <button type='submit'>Sign In</button>
+//             </div>
+
+//           </form>
+//         )}
+
+
+//         <div className="dont-have-an-account">
+//           <span>
+//             Don't have an account?
+//           </span>
+//           <Link to="/signup">Sign up!</Link>
+//         </div>
+//       </div>
+
+//     </article>
+//   );
+// }
+
+//NON-MFA VERSION
 export default function SigninPage() {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [mfa, setMfa] = React.useState('');
   const [errors, setErrors] = React.useState('');
-  const [code, setCode] = React.useState('');
-  const [mfaactive, setMfaactive] = React.useState(false);
-  const [gotuser, setGotuser] = React.useState();
-
-
-  const onsubmitcode = async (event) => {
-    setErrors('')
-    event.preventDefault();
-
-    var dummyuser = gotuser;
-
-    if(mfaactive == false) {
-    Auth.verifyTotpToken(dummyuser, mfa)
-      .then(() => {
-        // don't forget to set TOTP as the preferred MFA method
-        Auth.setPreferredMFA(gotuser, 'TOTP');
-        // gotuser.attributes.setMfaactive("true");
-        console.log("hey")
-      })
-      .catch((e) => {
-        setErrors(e.message)
-      });
-    }
-
-    try {
-      await  Auth.confirmSignIn(gotuser, mfa, 'SOFTWARE_TOKEN_MFA') ;
-      localStorage.setItem("access_token", gotuser.signInUserSession.accessToken.jwtToken)
-      window.location.href = "/"
-    } catch (error) {
-      setErrors(error.message)
-    }
-
-  }
 
   const onsubmit = async (event) => {
     setErrors('')
@@ -54,25 +198,8 @@ export default function SigninPage() {
 
     Auth.signIn(email, password)
       .then(user => {
-        setGotuser(user);
-        
-        try{
-        if(user.attributes["custom:mfa_active"]) {
-          setMfaactive(false);
-        } }
-        catch {
-          setMfaactive(true); 
-        }
-   
-        
-
-        Auth.setupTOTP(user).then((code) => {
-          console.log(code)
-          const str = "otpauth://totp/AWSCognito?secret=" + code;
-          setCode(str);
-          console.log(str);
-        });
-
+        localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+        window.location.href = "/"
       })
       .catch(error => {
         if (error.code == 'UserNotConfirmedException') {
@@ -91,9 +218,6 @@ export default function SigninPage() {
   const password_onchange = (event) => {
     setPassword(event.target.value);
   }
-  const mfa_onchange = (event) => {
-    setMfa(event.target.value);
-  }
 
   let el_errors;
   if (errors) {
@@ -105,73 +229,37 @@ export default function SigninPage() {
       <div className='signin-info'>
         <Logo className='logo' />
       </div>
-
-      {gotuser && mfaactive == false ? (
-        <QRCode value={code} />
-      ) : (
-        <></>
-      )}
-
       <div className='signin-wrapper'>
-
-        {!gotuser ? (
-          <form
-            className='signin_form'
-            onSubmit={onsubmit}
-          >
-            <h2>Sign into your Cruddur account</h2>
-            <div className='fields'>
-              <div className='field text_field username'>
-                <label>Email</label>
-                <input
-                  type="text"
-                  value={email}
-                  onChange={email_onchange}
-                />
-              </div>
-              <div className='field text_field password'>
-                <label>Password</label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={password_onchange}
-                />
-              </div>
+        <form
+          className='signin_form'
+          onSubmit={onsubmit}
+        >
+          <h2>Sign into your Cruddur account</h2>
+          <div className='fields'>
+            <div className='field text_field username'>
+              <label>Email</label>
+              <input
+                type="text"
+                value={email}
+                onChange={email_onchange}
+              />
             </div>
-            {el_errors}
-            <div className='submit'>
-              <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-              <button type='submit'>Sign In</button>
+            <div className='field text_field password'>
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={password_onchange}
+              />
             </div>
+          </div>
+          {el_errors}
+          <div className='submit'>
+            <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
+            <button type='submit'>Sign In</button>
+          </div>
 
-          </form>
-        ) : (
-          <form
-            className='signin_form'
-            onSubmit={onsubmitcode}
-          >
-            <h2>Sign into your Cruddur account</h2>
-            <div className='fields'>
-
-              <div className='field text_field username'>
-                <label>MFA Code</label>
-                <input
-                  type="mfa"
-                  value={mfa}
-                  onChange={mfa_onchange}
-                />
-              </div>
-            </div>
-            {el_errors}
-            <div className='submit'>
-              <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-              <button type='submit'>Sign In</button>
-            </div>
-
-          </form>
-        )}
-
-
+        </form>
         <div className="dont-have-an-account">
           <span>
             Don't have an account?
--- journal/week3.md
+++ journal/week3.md
@@ -1,6 +1,89 @@
 # Week 3 — Decentralized Authentication
 
-https://docs.amplify.aws/lib/auth/mfa/q/platform/js/
+
+## Homework Challenges
+
+- [x] Setup JWT Authentication in app.py
+  - Folled AB's video [Congito JWT Server side Verify](https://www.youtube.com/watch?v=d079jccoG-M)
+  - Created the [cognito_jwt_token.py](./../backend-flask/lib/cognito_jwt_token.py) file and added all the required functions
+  - Integrated [cognito_jwt_token.py](./../backend-flask/lib/cognito_jwt_token.py) into [app.py](./../backend-flask/app.py)
+    - ```
+      cognito_jwt_token = CognitoJwtToken(
+        user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+        user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+        region=os.getenv("AWS_DEFAULT_REGION")
+      )
+      ```
+  - Added an extra mock data entry to [home_activities.py](./../backend-flask/services/home_activities.py) which is returned when logged in
+    - Conditional logic:
+      - ```
+        if cognito_user_id != None:
+          extra_crud = {
+            ...
+          }
+        ```
+    - Screenshot showing extra mock data:
+        - ![extra mock data](./assets/week3/week3-extra-data.PNG)
+    
+- [x] Setup JWT Authentication using middleware
+ 
+- [x] Setup MFA with Google Auth App
+  - Follwed the guide [Multi-factor authentication](https://docs.amplify.aws/lib/auth/mfa/q/platform/js/) by Amplify Dev Center
+
+## Homework
+
+- [x] Setup Cognito User Pool
+  - Added a user pool using the Cognito interface:
+    - ![Cognito Interface](./assets/week3/week3-user-pool.PNG)
+  - I also added the required React en vars in the [docker-compose file](./../docker-compose.yml)
+- Cognito Frontend
+  - [x] Implement Custom Signin Page
+    - Added necessary code to [SinginPage.js](./../frontend-react-js/src/components/../pages/SigninPage.js)
+    - ```
+      Auth.signIn(email, password)
+        .then(user => {
+          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+          window.location.href = "/"
+        })
+      ```
+    - Error thrown by signin page showing it is working:
+      - ![signin page](./assets/week3/week3-signin.PNG)
+
+  - [x] Implement Custom Signup Page
+    - Added necessary code to [SingupPage.js](./../frontend-react-js/src/components/../pages/SignupPage.js)
+    - ```
+      const { user } = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          name: name,
+          email: email,
+          preferred_username: username,
+        },
+      ```
+    - Error thrown by signup page showing it is working:
+      - ![signup page](./assets/week3/week3-signup.PNG)
+  - [x] Implement Custom Confirmation Page
+    - Added necessary code to [ConfirmationPage.js](./../frontend-react-js/src/components/../pages/ConfirmationPage.js.js) 
+    - ```
+      await Auth.resendSignUp(email);
+      console.log('code resent successfully');
+      setCodeSent(true)
+      ```
+    - Error thrown by confirmation page showing it is working:
+      - ![confirmation page](./assets/week3/week3-confirm.PNG)
+  
+  - [x] Implement Custom Recovery Page
+    - Added necessary code to [RecoverPage.js](./../frontend-react-js/src/components/../pages/RecoverPage.js)
+    - ```
+      Auth.forgotPasswordSubmit(username, code, password)
+        .then((data) => setFormState('success'))
+      ```
+- [x] Watch about different approaches to verifying JWTs
+  - I took notes in the **Class Notes**  section below
+
+
+
 ## Class Notes
 
 ### _1. [LIVE] Decentralized Authenication Overview_
