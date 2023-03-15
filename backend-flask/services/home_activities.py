4b24f2cbc7f6a5e40a9536d5cc3351d903cb4e11
Felix Pitterling <f.pitterling@gmail.com>
2023-03-15 12:56:10 +0000

    Week 4 Code - SQL RDS Part II (not live)

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 # import logging
 
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
@@ -18,18 +18,31 @@ class HomeActivities:
     #   now = datetime.now(timezone.utc).astimezone()
     #   span.set_attribute("app.now", now.isoformat())
 
-    sql = """
-    SELECT * FROM activities
-    """
+ 
+    sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
     print(sql)
     with pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql)
         # this will return a tuple
         # the first field being the data
-        json = cur.fetchall()
+        json = cur.fetchone()
+    print("-------")
     print(json[0])
-    return "hello"
-    # return json[0]
-    # return results
+    return json[0]
     
