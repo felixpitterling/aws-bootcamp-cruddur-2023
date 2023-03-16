60a6f00d4df7a2564fe3f189e1dbf6157de17746
Felix Pitterling <f.pitterling@gmail.com>
2023-03-16 11:40:54 +0000

    Week 4 Code - Creating Activities Part I (not live)

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,21 +1,54 @@
 from psycopg_pool import ConnectionPool
 import os
 
+
 def query_wrap_object(template):
-  sql = f"""
+    sql = f"""
   (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
   {template}
   ) object_row);
   """
-  return sql
+    return sql
+
 
 def query_wrap_array(template):
-  sql = f"""
+    sql = f"""
   (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
   {template}
   ) array_row);
   """
-  return sql
+    return sql
+
+
+def print_sql_err(err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+
+    # print the connect() error
+    print("\npsycopg ERROR:", err, "on line number:", line_num)
+    print("psycopg traceback:", traceback, "-- type:", err_type)
+
+    # psycopg2 extensions.Diagnostics object attribute
+    print("\nextensions.Diagnostics:", err.diag)
+
+    # print the pgcode and pgerror exceptions
+    print("pgerror:", err.pgerror)
+    print("pgcode:", err.pgcode, "\n")
+
+
+def query_commit():
+    try:
+        conn = pool.connection()
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+    except Exception as err:
+        print_sql_err(err)
+        # conn.rollback()
+
 
 connection_url = os.getenv("CONNECTION_URL")
 pool = ConnectionPool(connection_url)
