11e8c760fb98d7043a0c9e50609c85a3b2243ad3
Felix Pitterling <f.pitterling@gmail.com>
2023-06-13 09:50:54 +0000

    Week X - Cleanup - updated seed query

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -11,4 +11,10 @@ VALUES
     (SELECT uuid from public.users WHERE users.handle = 'felix' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'alex' LIMIT 1),
+    'I am the other!',
+    current_timestamp + interval '10 day'
+  );
+
