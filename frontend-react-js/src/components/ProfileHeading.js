0b59aa3edfb6eb87b365aa8bbb3bfa3b91edcf55
Felix Pitterling <f.pitterling@gmail.com>
2023-07-30 14:19:09 +0000

    More changes

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -11,8 +11,8 @@ export default function ProfileHeading(props) {
     backgroundPosition: 'center',
   };
 
-  const premium_url = process.env.REACT_APP_FRONTEND_URL + "/payment"
-  console.log(props.profile.premium_status)
+  const premium_url = `${process.env.REACT_APP_FRONTEND_URL}/payment`
+
   return (
   <div className='activity_feed_heading profile_heading'>
     <div className='title'>{props.profile.display_name}</div>
@@ -28,8 +28,8 @@ export default function ProfileHeading(props) {
       <EditProfileButton setPopped={props.setPopped} />
     </div>
     <div className="bio">{props.profile.bio}</div>
-    <p className="status">Account Status: {props.profile.premium_status ? "Premium Account" : "Free Account"}</p>
-    {props.profile.premium_status ? null : <a href={premium_url}><button className="premiumButton">Get Premium</button></a>}
+    <p className="status">Account Status: {props.profile.premium_status == true ? "Premium Account" : "Free Account"}</p>
+    {props.profile.premium_status == true ? null : <a href={premium_url}><button className="premiumButton">Get Premium</button></a>}
   </div>
   );
 }
